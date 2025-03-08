63b3dac94d8bdf25602b9534bd4ae9732778cbc6
zakusenSS <cycpp866@yahoo.co.jp>
2025-03-08 13:43:04 +0900

    非表示で投稿した内容をブラウザ上で表示

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -7,7 +7,15 @@ function post (){
     XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
-      console.log(XHR.response.article);
+      const item = XHR.response.article;
+      const contentsArea = document.getElementById("contents_area");
+      const articleText = document.getElementById("article_text");
+      const HTML = `
+        <div class="article">
+          ${item.text }
+        </div>`;
+      contentsArea.insertAdjacentHTML("afterbegin", HTML);
+      articleText.value = "";
     };
     e.preventDefault();
   });
